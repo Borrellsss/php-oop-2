8143eedcc90785f5609a035993e07ff2724375b5
Borrellsss <edoardofilippo.borrello@gmail.com>
2022-07-28 16:17:40 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+// L'e-commerce vende prodotti per gli animali.
+// I prodotti saranno oltre al cibo, anche giochi, cucce, etc.
+// L'utente potrà sia comprare i prodotti senza registrarsi, oppure
+// iscriversi e ricevere il 20% di sconto su tutti i prodotti.
+?>
